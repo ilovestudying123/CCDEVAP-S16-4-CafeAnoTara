a27d7274269355b5ba9583daf192783f30ed44df
Kaithlyn <kaithlyn_isabelle_m_casillan@dlsu.edu.ph>
2026-06-17 12:37:29 +0800

    header js

---
+++ frontend/resources/js/script-header-user.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("../../includes/header-user.html")
+        .then(response => {
+            if (!response.ok) throw new Error("Header file not found");
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading header:", error));
+});
