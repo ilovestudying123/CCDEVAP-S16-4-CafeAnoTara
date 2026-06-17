f3ae6e2e67078e357de843059867693fc03225fb
Kaithlyn <kaithlyn_isabelle_m_casillan@dlsu.edu.ph>
2026-06-17 14:46:11 +0800

    cafe array and details jd

---
+++ frontend/resources/js/cafe-array.js
@@ -0,0 +1,81 @@
+const cafes =[
+    {
+        id: "CafeW",
+        name: "Cafe W",
+        address: "123 Main Street, Bonifacio Global City, Taguig City",
+        wifiSpeed: "100Mbps",
+        outlets: 8,
+        priceRange: "100-300",
+        noiseLevel: "Moderate",
+        hours: "7:30am - 10:00pm",
+        rating: "3.6/5",
+        mainImage: "../../resources/imgs/cafe.jpg",
+        additionImages: [
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg"
+        ],
+        description: "dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+
+    {
+        id: "CafeX",
+        name: "Cafe X",
+        address: "1234 Main Street, Bonifacio Global City, Taguig City",
+        wifiSpeed: "10Mbps",
+        outlets: 10,
+        priceRange: "100-500",
+        noiseLevel: "Quiet",
+        hours: "7:30am - 9:00pm",
+        rating: "4.6/5",
+        mainImage: "../../resources/imgs/cafe.jpg",
+        additionImages: [
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg"
+        ],
+        description: "dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+
+    {
+        id: "CafeY",
+        name: "Cafe Y",
+        address: "12345 Main Street, Bonifacio Global City, Taguig City",
+        wifiSpeed: "1Mbps",
+        outlets: 6,
+        priceRange: "100-200",
+        noiseLevel: "Noisy",
+        hours: "8:00am - 10:00pm",
+        rating: "4.0/5",
+        mainImage: "../../resources/imgs/cafe.jpg",
+        additionImages: [
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg"
+        ],
+        description: "dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+
+    {
+        id: "CafeZ",
+        name: "Cafe Z",
+        address: "123456 Main Street, Bonifacio Global City, Taguig City",
+        wifiSpeed: "1Gbps",
+        outlets: 100,
+        priceRange: "100-1000",
+        noiseLevel: "Moderate",
+        hours: "8:00am - 10:00pm",
+        rating: "4.8/5",
+        mainImage: "../../resources/imgs/cafe.jpg",
+        additionImages: [
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg",
+            "../../resources/imgs/cafe.jpg"
+        ],
+        description: "dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+]
