cd59a6bc267028b987714f66c395637403aa354f
Kaithlyn <kaithlyn_isabelle_m_casillan@dlsu.edu.ph>
2026-07-14 14:33:42 +0800

    functions for cafe user dashboard, search, and images

---
+++ backend/models/cafeModel.php
@@ -0,0 +1,66 @@
+<?php
+class cafeModel {
+
+// Get cafe from cafe table, join rating from review table, get average raiting, and group by cafe id
+public function getCafeId ($conn, $cafe_id) {
+    $stmt = mysqli_prepare ($conn,
+        "SELECT c.*, AVG(r.rating) AS average_rating
+        FROM Cafes c
+        LEFT JOIN Review r ON c.cafe_id=r.cafe_id
+        WHERE c.cafe_id = ?
+        GROUP BY c.cafe_id"
+    );
+    mysqli_stmt_bind_param($stmt, "i", $cafe_id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    return mysqli_fetch_assoc($result);
+}
+
+// get cafe images from cafeimg table
+public function getCafeImages ($conn, $cafe_id) {
+    $stmt = mysqli_prepare ($conn, 
+        "SELECT photo_url FROM CafeIMG WHERE cafe_id = ?"
+    );
+    mysqli_stmt_bind_param($stmt, "i", $cafe_id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+public function getTopCafes ($conn, $limit = 4) {
+    $stmt = mysqli_prepare ($conn, 
+        "SELECT c.*, AVG(r.rating) as average_rating,
+            MIN(ci.photo_url) as main_image
+        FROM Cafes c
+        LEFT JOIN Review r ON c.cafe_id = r.cafe_id
+        LEFT JOIN CafeIMG ci ON c.cafe_id = ci.cafe_id
+        WHERE c.is_verified = TRUE
+        GROUP BY c.cafe_id
+        ORDER BY average_rating DESC
+        LIMIT ?"
+    );
+    mysqli_stmt_bind_param($stmt, "i", $limit);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+public function searchCafe ($conn, $name) {
+    $stmt = mysqli_prepare ($conn, 
+        "SELECT c.*, AVG(r.rating) as average_rating,
+            MIN(ci.photo_url) as main_image
+        FROM Cafes c
+        LEFT JOIN Review r ON c.cafe_id = r.cafe_id
+        LEFT JOIN CafeIMG ci ON c.cafe_id = ci.cafe_id
+        WHERE c.cafe_name = ? AND c.is_verified = TRUE
+        GROUP BY c.cafe_id"
+    );
+    mysqli_stmt_bind_param($stmt, "s", $name);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+
+}
+?>
