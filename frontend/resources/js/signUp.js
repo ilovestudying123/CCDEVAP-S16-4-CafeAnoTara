83e32521967b5cb22f48ad7e107086f16e55fa38
Pat <patricia_valcos@dlsu.edu.ph>
2026-07-14 17:06:01 +0800

    Added show password in sign Up but to be fixed

--- frontend/resources/js/signUp.js
+++ frontend/resources/js/signUp.js
@@ -79,3 +79,29 @@ function saveAccount() {
         window.location.href = "../user/dashboard.html";
     }
 }
+
+function togglePassword() {
+    const password = document.getElementById("password");
+    const icon = document.querySelector(".toggle-password i");
+
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+    }
+}
+
+function togglePasswordconf() {
+    const password = document.getElementById("confpasword");
+    const icon = document.querySelector(".toggle-passwordconf i");
+
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+    }
+}
