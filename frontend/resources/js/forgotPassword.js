ccaf42e6469ee4f63ec801869d54a1ab1456d352
Pat <patricia_valcos@dlsu.edu.ph>
2026-07-14 16:07:07 +0800

    Enable showing the password + added new JS

---
+++ frontend/resources/js/forgotPassword.js
@@ -0,0 +1,38 @@
+
+function checkUser() {   
+    if (document.getElementById("username").value === "customer@gmail.com" && document.getElementById("password").value === "customer123") {
+        window.location.href = "../user/dashboard.html";
+    } else if (document.getElementById("username").value === "owner@gmail.com" && document.getElementById("password").value === "owner123") {
+        window.location.href = "../owner/dashboard.html";
+    } else if (document.getElementById("username").value === "admin@gmail.com" && document.getElementById("password").value === "admin123") {
+        window.location.href = "../admin/dashboard.html";
+    } else {
+        alert("Sorry this account doesn't exist. Please try again.");
+    }
+}
+
+function togglePasswordnew() {
+    const password = document.getElementById("newpasword");
+    const icon = document.querySelector(".toggle-passwordnew i");
+
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+    }
+}
+
+function togglePasswordconf() {
+    const password = document.getElementById("confnewpasword");
+    const icon = document.querySelector(".toggle-passwordconf i");
+
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+    }
+}
