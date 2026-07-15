8070beefa34d53255b2e136e0dfadc15168a2bbc
yanchi24 <ian_lopez@dlsu.edu.ph>
2026-07-15 21:16:04 +0800

    Updated Categories Page

--- frontend/pages/admin/categories.php
+++ frontend/pages/admin/categories.php
@@ -1,5 +1,9 @@
 <?php
-    require "../../../backend/config/connection.php";
+require_once "../../../backend/config/connection.php";
+require_once "../../../backend/controllers/admin/categories-reports.php";
+
+$categoriesController = new CategoriesController($conn);
+$categories = $categoriesController->getCategories();
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="../../resources/css/header-style.css">
-    <link rel="stylesheet" href="../../resources/css/admin-dashboard.css">
-
+    <link rel="stylesheet" href="../../resources/css/admin-categories.css">
 </head>
 
 <body>
@@ -19,7 +22,77 @@
     </div>
 
     <div class="body-box">
+        <div class="search-section">
+            <h1>Report Categories</h1>
+            <button class="add-btn" onclick="openCreateModal()">
+                Add Category
+            </button>
+        </div>
+
+        <div class="table-wrapper">
+            <table id="categoriesTable">
+                <thead>
+                    <tr>
+                        <th>Category</th>
+                        <th width="180">Actions</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                <?php foreach ($categories as $category): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($category['report']) ?></td>
+                        <td>
+                            <div class="action-btn">
+                                <button class="hidden-btn" onclick="openEditModal(<?= $category['report_code'] ?>,
+                                    '<?= htmlspecialchars($category['report'], ENT_QUOTES) ?>')">
+                                    <img src="../../resources/imgs/edit-btn.png"></button>
+                                <button class="hidden-btn" onclick="deleteCategory(<?= $category['report_code'] ?>)">
+                                    <img src="../../resources/imgs/delete.png"></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+        <div id="createModal" class="modal">
+            <div class="create-modal-content">
+                <span class="close" onclick="closeCreateModal()">&times;</span>
+                <h2>Add Report Category</h2>
+                <form id="createCategoryForm">
+                    <div class="field">
+                        <label>Category Name</label>
+                        <input type="text" id="create-report" placeholder="Enter category" required>
+                    </div>
+
+                    <div class="button-container">
+                        <button type="button" class="add-btn" onclick="createCategory()">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
         
+        <div id="editModal" class="modal">
+            <div class="create-modal-content">
+                <span class="close" onclick="closeEditModal()">&times;</span>
+
+                <h2>Edit Report Category</h2>
+
+                <form id="editCategoryForm">
+                    <div class="field">
+                        <label>Category Name</label>
+                        <input type="text" id="edit-report" required>
+                    </div>
+
+                    <div class="button-container">
+                        <button  type="button" class="add-btn" onclick="updateCategory()"> Save </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
+    <script src="../../resources/js/admin-categories.js"></script>
 </body>
 </html>
