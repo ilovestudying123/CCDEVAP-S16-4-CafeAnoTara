1c70a5e59011bf6ff7339d6b9bb247460975b0a4
Kaithlyn <kaithlyn_isabelle_m_casillan@dlsu.edu.ph>
2026-07-23 11:15:38 +0800

    fix nav for review

--- frontend/includes/header-user.php
+++ frontend/includes/header-user.php
@@ -12,7 +12,7 @@ $firstname = isset($_SESSION['firstname']) ? $_SESSION['firstname'] : '';
     <nav class="nav-user">
         <a href="../../../backend/controllers/user/cafeController.php?action=dashboard" class="home">Home</a> |
         <a href="../../../backend/controllers/user/bookmarkController.php?action=bookmarks" class="bookmark">Bookmarks</a> |
-        <a href="../../../backend/controllers/user/reviewController.php?action=myReviews" class="review">My Reviews</a> |
+        <a href="../../../backend/controllers/user/reviewController.php?action=myReviews" class="reviews">My Reviews</a> |
         <a href="accountSettings.php">Account Settings</a>
     </nav>
 </div>
