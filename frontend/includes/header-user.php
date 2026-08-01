288d641a5a3c93675a378fdc68ef7a986a67086e
Pat <patricia_valcos@dlsu.edu.ph>
2026-08-01 22:17:47 +0800

    Color changes in account settings nav

--- frontend/includes/header-user.php
+++ frontend/includes/header-user.php
@@ -13,6 +13,6 @@ $firstname = isset($_SESSION['firstname']) ? $_SESSION['firstname'] : '';
         <a href="dashboard.php" class="home">Home |</a> 
         <a href="bookmarks.php" class="bookmark">Bookmarks |</a>
         <a href="postedreviews.php" class="reviews">My Reviews |</a>
-        <a href="accountSettings.php">Account Settings</a>
+        <a href="accountSettings.php" class="accSetting">Account Settings</a>
     </nav>
 </div>
